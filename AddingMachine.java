b220bbf4ccd86d9720a1179c05853ad623e524a8
Bread442 <56202694+Bread442@users.noreply.github.com>
2019-10-06 09:09:24 +0800

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,30 +1,41 @@
 package cse360assign2;
 
+/* This is a class that can save the history of addition and subtraction operation
+ * and translate into a string using toString()
+ */
 public class AddingMachine {
 
-	private int total;
+	private int total;		// Total value
+	private StringBuilder sb;	// History String
 	
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
+		sb = new StringBuilder("0");
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		sb.append(" + ");
+		sb.append(value);
 	}
 	
 	public void subtract (int value) {
+		total -= value;
+		sb.append(" - ");
+		sb.append(value);
 		
 	}
 		
 	public String toString () {
-		return "";
+		return sb.toString();
 	}
 
 	public void clear() {
-	
+		sb.delete(1, sb.length());
+		total = 0;
 	}
 }
